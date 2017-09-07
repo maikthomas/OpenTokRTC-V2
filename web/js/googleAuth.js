48e5224738052f029fd8b99f77205fcc22db1357
Mike Thomas <mthomas@tokbox.com>
2017-09-07 18:40:59 +0200

    Use dependency loader instead of script tag

--- web/js/googleAuth.js
+++ web/js/googleAuth.js
@@ -1,12 +1,17 @@
 !(function(exports) {
   var init = function(callback) {
-    gapi.load('auth2', function() {
-      gapi.auth2.init({
-        client_id: '128679106983-6pan3577kflbe08vr227fa96o1ft7jef.apps.googleusercontent.com',
-        hosted_domain: 'tokbox.com'
-      }).then(callback);
+    LazyLoader.dependencyLoad([
+      'https://apis.google.com/js/api.js'
+    ]).then(function() {
+      gapi.load('auth2', function() {
+        gapi.auth2.init({
+          client_id: '128679106983-6pan3577kflbe08vr227fa96o1ft7jef.apps.googleusercontent.com',
+          hosted_domain: 'tokbox.com'
+        }).then(callback);
+      });
     });
   };
+
 
   exports.GoogleAuth = {
     init: init
