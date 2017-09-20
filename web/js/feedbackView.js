879bff5d5a38b9165c35ca541b226c9f0d6364d9
Mike Thomas <mthomas@tokbox.com>
2017-09-20 12:04:35 +0200

    Add tab functionality to dummy feedback form

--- web/js/feedbackView.js
+++ web/js/feedbackView.js
@@ -2,11 +2,10 @@
   'use strict';
 
   var showFeedback,
+    container,
     sendButton,
-    audioScore,
-    videoScore,
-    otherInfo,
-    reportButton;
+    tabHeaders,
+    tabContents;
 
   var feedbackReportSelector = '.feedback-report';
 
@@ -20,39 +19,58 @@
   }
 
   var init = function() {
+    container = document.querySelector(feedbackReportSelector);
     showFeedback = document.querySelector('#showFeedback');
-    sendButton = document.querySelector('.feedback-report .send-feedback');
-    audioScore = document.querySelector('.feedback-report .audio-score');
-    videoScore = document.querySelector('.feedback-report .video-score');
-    otherInfo = document.querySelector('.feedback-report .other-info');
-    reportButton = document.querySelector('.feedback-report .report-issue');
+    tabHeaders = document.querySelectorAll('.feedback-report .tab-header');
+    tabContents = document.querySelectorAll('.feedback-report .tab-contents');
+    // sendButton = document.querySelector('.feedback-report .send-feedback');
+    // audioScore = document.querySelector('.feedback-report .audio-score');
+    // videoScore = document.querySelector('.feedback-report .video-score');
+    // otherInfo = document.querySelector('.feedback-report .other-info');
+    // reportButton = document.querySelector('.feedback-report .report-issue');
     addHandlers();
   };
 
   var resetForm = function() {
-    otherInfo.value = '';
+    // otherInfo.value = '';
   };
 
   var addHandlers = function() {
-    sendButton.addEventListener('click', function(event) {
-      event.preventDefault();
-
-      Utils.sendEvent('feedbackView:sendFeedback', {
-        audioScore: audioScore.options[audioScore.selectedIndex].value,
-        videoScore: videoScore.options[videoScore.selectedIndex].value,
-        description: otherInfo.value
+    tabHeaders.forEach(function(tabHeader) {
+      tabHeader.addEventListener('click', function(event) {
+        var activeHeader = event.currentTarget;
+        var currentTab = document.querySelector('#' + activeHeader.dataset.tab);
+        tabHeaders.forEach(function(header) {
+          header.classList.remove('active');
+        });
+        activeHeader.classList.add('active');
+        debugger;
+        tabContents.forEach(function(tab) {
+          debugger;
+          tab.classList.remove('active');
+        });
+        currentTab.classList.add('active');
       });
-
-      hideForm();
     });
+    // sendButton.addEventListener('click', function(event) {
+    //   event.preventDefault();
 
-    reportButton.addEventListener('click', function(event) {
-      event.preventDefault();
+    //   Utils.sendEvent('feedbackView:sendFeedback', {
+    //     audioScore: audioScore.options[audioScore.selectedIndex].value,
+    //     videoScore: videoScore.options[videoScore.selectedIndex].value,
+    //     description: otherInfo.value
+    //   });
 
-      Utils.sendEvent('feedbackView:reportIssue');
+    //   hideForm();
+    // });
 
-      hideForm();
-    });
+    // reportButton.addEventListener('click', function(event) {
+    //   event.preventDefault();
+
+    //   Utils.sendEvent('feedbackView:reportIssue');
+
+    //   hideForm();
+    // });
 
     showFeedback && showFeedback.addEventListener('click', function onShowFeedbackClicked(event) {
       event.preventDefault();
